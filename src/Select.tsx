aa699200fcdc0fbdbe15d25f538a08a48d344011
Nikeshkumar-tk <104181447+Nikeshkumar-tk@users.noreply.github.com>
2022-10-26 11:43:33 +0530

    Multi select added

--- src/Select.tsx
+++ src/Select.tsx
@@ -15,18 +15,24 @@ interface Props {
 const Select = ({ value, onChange, options }: Props) => {
   const [show, setShow] = useState(false)
   const [highLightedIndex, setHighLightedIndex] = useState(0)
+  const [selectedOptions, setSelectedOptions] = useState<SelectOptons[]>([])
   const clearOption = () => {
-    onChange(undefined)
+
+    setSelectedOptions([])
+
   }
 
   const selectOptions = (option: SelectOptons) => {
 
-   if(option === value)
-   {
-     
+
+    if(value == option){
+      
+    }
     onChange(option)
+    setSelectedOptions([...selectedOptions,option])
+    console.log(selectedOptions)
   
-  }
+
 
 
 
@@ -34,16 +40,26 @@ const Select = ({ value, onChange, options }: Props) => {
   const isOptionSelected = (option: SelectOptons) => {
     return option === value
   }
+  
   useEffect(() => {
     if(show){
       setHighLightedIndex(0)
     }
   },[show])
+
   return (
     <div className={styles.container} tabIndex={0} onClick={() => setShow(c => !c)}
       onBlur={() => setShow(false)}
     >
-      <span className={styles.value}> {value?.label} </span>
+      <span className={styles.value}> {
+      
+      selectedOptions.map((option) => (
+
+        <button>{option.label}</button>
+      ))
+      
+      
+      } </span>
       <button className={styles["close-btn"]} onClick={(e) => {
         e.stopPropagation()
         clearOption()
